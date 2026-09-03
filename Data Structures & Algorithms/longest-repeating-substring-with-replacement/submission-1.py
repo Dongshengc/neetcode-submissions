72aaaf32c5d9e330c630a286c058c41304eb45b9
Dongsheng Chen <33436815+Dongshengc@users.noreply.github.com>
2026-09-02 23:09:31 -0400

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        hashmap = {}
+        max_freq = 0
+        max_length = 0
+        left = 0
+
+        for right in range(len(s)):
+
+            if s[right] in hashmap:
+                hashmap[s[right]] += 1
+            else:
+                hashmap[s[right]] = 1
+
+            max_freq = max(max_freq, hashmap[s[right]])
+
+            if right - left + 1 - max_freq > k:
+                hashmap[s[left]] -= 1
+                left += 1
+
+            max_length = max(max_length, right - left +1)
+
+        return max_length
+
+
